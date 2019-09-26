7da630f8bee2af4ce9fa2f60d2af00682cc35f9e
kxwang <xinju.wang@portlandoregon.gov>
2019-09-26 15:32:50 -0700

    POWR-1351 Add address autocomplete

---
+++ web/modules/custom/portland/modules/portland_address_complete/portland_address_complete.js
@@ -0,0 +1,81 @@
+(function ($) {
+  var hinterXHR = new XMLHttpRequest();
+
+  function handleAddressInput(event) {
+    // retireve the input element
+    var value = $(event.target).val();
+    var option = $('#addresslist').find("[value='" + value + "']");
+
+    if (option.length > 0) {
+      $('#edit-field-address-or-entrance-0-address-locality').val(option.data("city"));
+      $('#edit-field-address-or-entrance-0-address-postal-code').val(option.data("zip"));
+
+      var dd = document.getElementById('edit-field-address-or-entrance-0-address-administrative-area');
+      for (var i = 0; i < dd.options.length; i++) {
+          if (dd.options[i].text.toLowerCase() === option.data("state").toLowerCase()) {
+              dd.selectedIndex = i;
+              break;
+          }
+      }
+    }
+  }
+
+  function handleAddressKeyup(event) {
+    // retireve the input element
+    var input = event.target;
+
+    // retrieve the datalist element
+    var addresslist = document.getElementById('addresslist');
+
+    // minimum number of characters before we start to generate suggestions
+    var min_characters = 3;
+
+    if (input.value.length < min_characters ) { 
+        return;
+    } else { 
+
+        // abort any pending requests
+        hinterXHR.abort();
+
+        hinterXHR.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+
+                // We're expecting a json response so we convert it to an object
+                var response = JSON.parse( this.responseText ); 
+
+                // clear any previously loaded options in the datalist
+                addresslist.innerHTML = "";
+
+                response.candidates.forEach(function(item) {
+                    // Create a new <option> element.
+                    var option = document.createElement('option');
+                    option.value = item.address;
+                    option.text = item.address;
+                    option.setAttribute('data-city', item.attributes.city);
+                    option.setAttribute('data-state', item.attributes.state);
+                    option.setAttribute('data-zip', item.attributes.zip_code);
+
+                    // attach the option to the datalist element
+                    addresslist.appendChild(option);
+                });
+            }
+        };
+
+        hinterXHR.open("GET", "https://www.portlandmaps.com/api/suggest/?alt_coords=1&api_key=F75A809DAF9012F7B28A4FB552FA5469&query=" + encodeURIComponent(input.value), true);
+        hinterXHR.send();
+    }
+  }
+
+  // On keyup, query PortlandMaps Suggest API
+  $( document ).ready(function() {
+    // Add a keyup event listener to our input element
+    var address_input = $('input.address-line1');
+    if(address_input) {
+      address_input.on("keyup", handleAddressKeyup);
+      address_input.on("input", handleAddressInput);
+      // address_input.attr('list', 'addresslist');
+      // address_input.after('<datalist id="addresslist"></datalist>');
+    }
+  });
+
+})(jQuery);
